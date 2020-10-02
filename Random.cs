2809bd78a9fa1b9848903f342ab118f0067d603d
duui <67356442+Duui3111@users.noreply.github.com>
2020-10-01 23:19:50 -0400

    Create Random.cs

---
+++ Random.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace random
+{
+    class program 
+    {
+       Public static void(string[], args)
+       {
+          Random _random = new Random();  
+
+          public int RandomNumber(int min, int max)  
+          {  
+               return _random.Next(min, max);  
+          } 
+           
+          Console.WriteLine(RandomNumber(8, 5)); 
+        }
+     }
+}
