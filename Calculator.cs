348db96bfab5032ee9adbb104de14b510b9e40f6
duui <67356442+Duui3111@users.noreply.github.com>
2020-10-01 20:49:12 -0400

    Create Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Calculator
+{
+    class Program
+    {     
+        static void Main(string[] args)
+        {   
+            Console.Write("Calculator: ");
+
+            string input = Console.ReadLine();
+
+            double num1 = double.Parse(input.Split(" ")[0]);
+            double num2 = double.Parse(input.Split(" ")[2]);
+
+            string operation = input.Split(" ")[1];
+
+            if(operation == "+") Console.WriteLine($"result: " + (num1 + num2));
+        
+            else if(operation == "/") Console.WriteLine($"result: " + (num1 / num2));
+                   
+            else if(operation == "*") Console.WriteLine($"result: " + (num1 * num2));
+                    
+            else if(operation == "-") Console.WriteLine($"result: " + (num1 - num2));
+            
+            Console.ReadKey();
+        }
+    }
+}
